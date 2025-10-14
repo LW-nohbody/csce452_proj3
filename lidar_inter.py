a713a1a7cb159c9dd66cbb2cb8a4815f841b3c1d
LW-nohbody <lance.wiley09@tamu.edu>
2025-10-13 19:40:56 -0500

    Added files

---
+++ lidar_inter.py
@@ -0,0 +1,158 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+import numpy as np
+import math
+from sklearn.cluster import DBSCAN
+
+
+#TODO: Determine the sections from the LIDAR that are dynamic
+    #NOTE: Can separate dynamic sections by comparing with a static map taken as initial measurement
+    #NOTE: Update the past range as a rolling avg? (need to deal with flickering wall values)
+class Lidar_Inter(Node):
+    def __init__(self):
+        super().__init__('interface')
+        self.lidar = self.create_subscription(LaserScan, '/scan', self.getVals, 10)
+        self.marker_push = self.create_publisher(PoseArray, '/moved_positions', 10)
+        self.lidar_ranges: list[list[float]] = []
+        self.past_lidar_range: list[list[float]] = []
+        self.twice_past_range: list[list[float]] = [] #DEBUG: REMOVE maybe
+        # self.move_threshold = 0.05 #m
+        # self.eps = 0.75
+        # self.points_for_group = 5 #TODO: fine tune, keep high to cut out noise, low enough so we get all people movement (has issue when far from lidar)
+        # self.group_threshold = 10 #Min size of group to count as a person
+        self.move_threshold = 0.05 #m
+        self.eps = 0.2
+        self.points_for_group = 3 #TODO: fine tune, keep high to cut out noise, low enough so we get all people movement (has issue when far from lidar)
+        self.group_threshold = 5 #Min size of group to count as a person
+
+    
+    def pointPub(self, point_to_pub:list[Point]):
+        self.get_logger().info("Publishing different points")
+        pose_arr:list[Pose] = []
+        for point in point_to_pub:
+            pose_arr.append(Pose(position=point))
+        msg = PoseArray()
+        msg.poses = pose_arr[:]
+        self.marker_push.publish(msg)
+
+    def getVals(self, msg):
+        max_range = msg.range_max
+        min_range = msg.range_min
+        max_angle = msg.angle_max #TODO: if no use, delete
+        min_angle = msg.angle_min
+        angle_step = msg.angle_increment
+        self.lidar_ranges = []
+        for i in range(len(msg.ranges)):
+            if(math.isnan(msg.ranges[i]) or msg.ranges[i] < min_range or msg.ranges[i] > max_range): #Filter out values outside of range
+                self.lidar_ranges.append(None)
+                continue 
+            r = msg.ranges[i]
+            theta = min_angle + angle_step*i
+            self.lidar_ranges.append(polarToCartesian(r, theta))
+        
+        #TODO: filter out the stationary points
+        # if(self.past_lidar_range == []):
+        #     self.past_lidar_range = self.lidar_ranges[:]
+        #     return 
+        if(self.twice_past_range == []):
+            self.twice_past_range = self.past_lidar_range[:]
+            self.past_lidar_range = self.lidar_ranges[:]
+            return 
+        else:
+            diff_points: list[list [float]] = []
+
+            for i in range(min(len(self.lidar_ranges), len(self.past_lidar_range))):
+                if(self.lidar_ranges[i] == None): continue
+                elif(self.past_lidar_range[i] == None):
+                    continue
+                    dist = self.move_threshold + 1 #Movement detected, set distance above the threshold
+                    decrease_in_r = 0
+                elif(self.twice_past_range[i] == None): #DEBUG: REMOVE Maybe
+                    continue
+                else: 
+                    dist = math.sqrt((self.lidar_ranges[i][1] - self.past_lidar_range[i][1])**2 + (self.lidar_ranges[i][0] - self.past_lidar_range[i][0])**2)
+                    dist_2 = math.sqrt((self.lidar_ranges[i][1] - self.twice_past_range[i][1])**2 + (self.lidar_ranges[i][0] - self.twice_past_range[i][0])**2) #DEBUG: remove maybe
+                    decrease_in_r = cartToPolar(self.past_lidar_range[i][0], self.past_lidar_range[i][1])[0] - cartToPolar(self.lidar_ranges[i][0], self.lidar_ranges[i][1])[0]
+                is_new_value = (abs(dist) > self.move_threshold) and (abs(dist_2 > self.move_threshold)) and (decrease_in_r >= 0) #Has point moved further than the threshold distance and is closer than the previous point on that line?
+                if(is_new_value):
+                    if(self.lidar_ranges[i] == None): self.get_logger().info("ERROR: Appending NONE value")
+                    elif(self.lidar_ranges[i][0] == float('nan')): self.get_logger().info("ERROR: contains NONE value")
+                    diff_points.append(self.lidar_ranges[i])
+            self.get_logger().info(f"got diffs, {len(diff_points)}") #TODO: Remove
+            for diff in diff_points:
+                self.get_logger().info(f"Difference: ({diff[0]}, {diff[1]})") #TODO:REMOVE
+            self.twice_past_range = self.past_lidar_range[:] #DEBUG: REMOVE maybe
+            self.past_lidar_range = self.lidar_ranges[:]
+            
+            
+            #Convert to Point array
+            point_arr: list[Point] = []
+            for point in diff_points:
+                point_arr.append(Point(x=point[0], y=point[1], z=0.0))
+            
+            grouped_points = self.groupPoints(point_arr)
+            self.pointPub(grouped_points)
+    
+
+    #TODO: May want to move this function to lidar_inter.py (fits better)
+    def groupPoints(self, points: list[Point]): 
+        temp: list[Point] = points[:]
+        temp_list = []
+        for i in temp:
+            temp_list.append([i.x, i.y])
+        if(temp_list == []):
+            return []
+        list_np = np.array(temp_list)
+        db = DBSCAN(eps=self.eps, min_samples=self.points_for_group)
+        fitted_list = db.fit(list_np)
+        #NOTE: fitted_list.labels_ will give me a list with the associated labels for each point
+        
+        seen_groups = {}
+        group_points: list[Point] = []
+        for i in range(len(temp)):
+            label = fitted_list.labels_[i]
+            num_occurences = np.sum(fitted_list.labels_ == label)
+            if(label == -1): continue #noise
+            if (seen_groups.get(label) == None) and (num_occurences > self.group_threshold): #First time seeing this group and group is of large enough size
+                seen_groups.update({label: True})
+                group_points.append(temp[i])
+        
+        self.get_logger().info(f"num groups: {len(group_points)}")
+        for point in group_points:
+            self.get_logger().info(f"Point: ({point.x}, {point.y})") #TODO: REMOVE
+        
+        return group_points
+
+    
+
+
+
+def polarToCartesian(r: float, theta: float):
+    x:float = r * math.cos(theta)
+    y:float = r * math.sin(theta) #Why are the y-values negated?
+    return [x, y]
+
+def cartToPolar(x:float, y:float):
+    r = math.sqrt(x**2 + y**2)
+    if(x == 0): theta = math.pi/2 * (y/(abs(y)))
+    theta = math.atan(y/x)
+    return [r, theta]
+
+def main():
+    rclpy.init()
+
+    sensor = Lidar_Inter()
+
+
+    rclpy.spin(sensor)
+
+    sensor.destroy_node()
+
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ marker.py
@@ -0,0 +1,177 @@
+import rclpy
+from rclpy.node import Node
+from visualization_msgs.msg import MarkerArray
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseArray
+import math
+
+
+
+# tracks person position
+class Person():
+    def __init__(self, id: int, curr_pos: Point):
+        self.id = id 
+        self.curr_pos = Point()
+        self.pos: list [Point] = []
+        self.updatePos(curr_pos)
+    
+    def updatePos(self, new_pos:Point): #Creates a copy of the point and appends that to the array
+        temp = Point()
+        temp.x = new_pos.x 
+        temp.y = new_pos.y 
+        temp.z = new_pos.z
+        self.curr_pos = temp
+        self.pos.append(temp)
+
+# Group the dynamic code sections into people and pick a point in there to create marker for (can use DBSCAN)
+class Sim_Marker(Node):
+    def __init__(self):
+        super().__init__('marker')
+        self.marker_pub = self.create_publisher(MarkerArray, '/sim_markers', 10)
+        self.point_sub = self.create_subscription(PoseArray, '/moved_positions', self.translate, 10)
+        self.timer = self.create_timer(1.0, self.publishMarkers)
+        self.marker_arr = []
+        self.point_arr: list[Point] = []
+        self.people: list[Person] = []
+        self.curr_id = 0
+    
+    def publishMarkers(self):
+        # self.get_logger().info(f"Creating {len(self.marker_arr)} markers for {len(self.people)} people") #TODO: REMOVE
+        markerList = MarkerArray()
+        markerList.markers = self.marker_arr
+        self.marker_pub.publish(markerList)
+    
+    def translate(self, msg):
+        # Gets point of groups with significant movement
+        self.point_arr = []
+        pose_arr = msg.poses
+        for i in pose_arr:
+            self.point_arr.append(i.position)
+
+        grouped_points = self.point_arr[:]
+
+        # Handles first seen case
+        if(self.people == []):
+            for group in grouped_points:
+                self.get_logger().info(f"Creating new person with {self.curr_id} id for point ({group.x}, {group.y})") #TODO:REMOVE
+                self.people.append(Person(self.curr_id, group))
+                self.curr_id += 1
+        else:
+            #Find the person closest to each group and assign it to them
+            temp_people = self.people[:]
+            assigned_groups: list[Point] = []
+            for group in grouped_points:
+                closest_p = None
+                closest_dist = -1
+                self.get_logger().info(f"temp_people size: {len(temp_people)}, self.people size: {len(self.people)}") #TODO:REMOVE
+                for p in temp_people:
+                    if closest_dist == -1:
+                        closest_p = p
+                        closest_dist = getDist(group, p.curr_pos)
+                    elif (getDist(group, p.curr_pos) < closest_dist):
+                        closest_p = p 
+                        closest_dist = getDist(group, p.curr_pos)
+                if(closest_p == None): continue
+                temp_people.remove(closest_p)
+                p.updatePos(group)
+                self.get_logger().info(f"Updating person {p.id} with point: ({group.x}, {group.y}), for {len(p.pos)} point total") #TODO: REMOVE
+                assigned_groups.append(group)
+            
+            if(len(assigned_groups) < len(grouped_points)): #Handles new people
+                for group in grouped_points:
+                    if not (group in assigned_groups):
+                        self.get_logger().info(f"Creating new person with {self.curr_id} id for point ({group.x}, {group.y})") #TODO: REMOVE
+                        self.people.append(Person(self.curr_id, group))
+                        self.curr_id += 1
+                        assigned_groups.append(group)
+
+        # #TODO: Only make a marker for those groups that have existed for at least x frames
+        # temp_people = self.people[:]            
+        # for p in self.people:
+        #     if(len(p.pos) < 2):
+        #         temp_people.remove(p)
+            
+        #clear marker array and repopulate it, TODO: Should this only occur the second time the person is marked???
+        self.marker_arr = []
+        for p in self.people:
+            self.createMarker(p)
+        
+        self.publishMarkers()
+        
+    
+    def createMarker(self, person:Person):
+        marker = Marker()
+        marker.header.frame_id = "/laser"
+        marker.header.stamp = Node.get_clock(self).now().to_msg()
+        
+        marker.ns = "person"
+        marker.id = person.id
+
+        marker.type = Marker.LINE_STRIP
+        marker.action = Marker.ADD
+
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = 0.0
+        marker.pose.position.z = 0.0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
+
+        marker.scale.x = 0.01
+        marker.scale.y = 0.01
+        marker.scale.z = 0.0
+
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.color.a = 1.0
+
+        marker.points = person.pos
+
+        marker.lifetime.sec = 0
+
+        self.marker_arr.append(marker)
+
+
+def getDist(point1:Point, point2:Point):
+    return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+
+def main():
+    rclpy.init()
+
+    mark = Sim_Marker()
+
+    # startPoint = Point()
+    # startPoint.x = 0.0
+    # startPoint.y = 0.0
+    # startPoint.z = 0.0
+    # test = Person(0, startPoint)
+
+
+    # testPoint = Point()
+    # testPoint.x = 2.49
+    # testPoint.y = -1.65
+    # #R = 2.987
+    # #theta = -0.58
+    # test.updatePos(testPoint)
+
+    # #R = 2.09 theta = -1.847
+    # testPoint.x = -0.57
+    # testPoint.y = -2.01
+
+    # test.updatePos(testPoint)    
+    
+
+    # mark.createMarker(test)
+
+    rclpy.spin(mark)
+
+    mark.destroy_node()
+
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+     
