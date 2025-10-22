0a0275b8a26e6445485c8330a9b0bff0d675739d
LW-nohbody <lance.wiley09@tamu.edu>
2025-10-22 14:49:28 -0500

    Added shutdown when the bag playback ends

--- project3_tracker.py
+++ project3_tracker.py
@@ -1,32 +1,48 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler, Shutdown
 from launch.substitutions import LaunchConfiguration
+from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='csce452_proj3',
-            executable='lidar',
-            name='lidar'
-        ),
-        Node(
-            package='csce452_proj3',
-            executable='marker',
-            name='marker'
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', '/home/nohbody/.rviz2/project_3_config.rviz'],
-        ),
-        DeclareLaunchArgument(
-            'bag_in',
-            description='Path to the ros2 bag to play'
-        ),
-        #TODO: add argument to direct the output for the bag recordings for later and change argument names
-        ExecuteProcess(
-            cmd=['ros2', 'bag', 'play', LaunchConfiguration('bag_in')]
+    node1 = Node(
+        package='csce452_proj3',
+        executable='lidar',
+        name='lidar'
+    )
+    node2 = Node(
+        package='csce452_proj3',
+        executable='marker',
+        name='marker'
+    )
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', '/home/nohbody/.rviz2/project_3_config.rviz'],
+    )
+    arg1 = DeclareLaunchArgument(
+        'bag_in',
+        description='Path to the ros2 bag to play'
+    )
+    playback = ExecuteProcess(
+        cmd=['ros2', 'bag', 'play', LaunchConfiguration('bag_in')]
+    )
+
+    #TODO: Add arguments and command to record a bag
+
+    shutdown_on_exit = RegisterEventHandler(
+        OnProcessExit(
+            target_action=playback,
+            on_exit=[Shutdown()]
         )
+    )
+
+    return LaunchDescription([
+        node1,
+        node2,
+        rviz,
+        arg1,
+        playback,
+        shutdown_on_exit
     ])
